6e3425ffe5d68fa88aadfd6954049b1f0f601d57
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-02-01 14:17:01 -0600

    fixing href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
           <p>hannah@bradleyclean.com<br/>(715) 642-5924</p>
         </div>
         <div style={{textAlign: "center", marginBottom: "10px"}}>
-          <img onClick={() => href("https://www.instagram.com/hannahirene4/")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/717/717392.png" alt="instagram" />
-          <img onClick={() => href("https://www.facebook.com/hannah.poppe.96")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/2175/2175193.png" alt="facebook" />
-          <img onClick={() => href("https://twitter.com/hannahpoppe25")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/733/733635.png" alt="twitter" />
+          <a href="https://www.instagram.com/hannahirene4/"><img className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/717/717392.png" alt="instagram" /></a>
+          <a href="https://www.facebook.com/hannah.poppe.96"><img className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/2175/2175193.png" alt="facebook" /></a>
+          <a href="https://twitter.com/hannahpoppe25"><img className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/733/733635.png" alt="twitter" /></a>
         </div>
         <p className="col info-footer text-center">
           Copyright 2023 - Website designed and created by Natalie Poppe ®
