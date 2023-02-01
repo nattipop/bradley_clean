3704d1c8c3d79f0437ea286e877ac0f025a646c8
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-02-01 14:10:21 -0600

    use Href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
-import { useNavigate } from "react-router";
+import { useHref } from "react-router";
 
 const Footer = () => {
-  const navigate = useNavigate();
+  const href = useHref();
   return (
     <div id="footer">
       <div className="container" style={{width: "100vw", paddingBottom: "60px"}}>
@@ -9,9 +9,9 @@ const Footer = () => {
           <p>hannah@bradleyclean.com<br/>(715) 642-5924</p>
         </div>
         <div style={{textAlign: "center", marginBottom: "10px"}}>
-          <img onClick={() => navigate("https://www.instagram.com/hannahirene4/")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/717/717392.png" alt="instagram" />
-          <img onClick={() => navigate("https://www.facebook.com/hannah.poppe.96")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/2175/2175193.png" alt="facebook" />
-          <img onClick={() => navigate("https://twitter.com/hannahpoppe25")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/733/733635.png" alt="twitter" />
+          <img onClick={() => href("https://www.instagram.com/hannahirene4/")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/717/717392.png" alt="instagram" />
+          <img onClick={() => href("https://www.facebook.com/hannah.poppe.96")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/2175/2175193.png" alt="facebook" />
+          <img onClick={() => href("https://twitter.com/hannahpoppe25")} className="footer-icon" src="https://cdn-icons-png.flaticon.com/512/733/733635.png" alt="twitter" />
         </div>
         <p className="col info-footer text-center">
           Copyright 2023 - Website designed and created by Natalie Poppe ®
